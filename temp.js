7bea868297257ed1a710cb84198d7ade51d00bfa
arohick11 <arohichakote@gmail.com>
2025-05-05 16:49:56 +0530

    Add files via upload

---
+++ temp.js
@@ -0,0 +1,38 @@
+let name = "John Doe";
+let age = 30;
+let x = 5;
+let y = 10e40;
+let z = "Hello";
+let isTrue = true;
+let arr = [1,2,3,4,5];
+let obj = {name: "John", age: 30};
+let func = function() {return "Hello World";};
+let nullValue = null;
+let undefinedvalue;
+let symbolValue = Symbol("w");
+let bigIntValue = BigInt(123456789012345678901234567890);
+let dateValue = new Date();
+let demo = `The sum of number is ${3 + 5}`
+
+console.log(demo);
+console.log("x:",x);
+console.log("y:",y);
+console.log("y:",y);
+console.log("z:",z);
+console.log("isTrue:",isTrue);
+console.log("arr:",arr);
+console.log("obj:",obj);
+console.log("func:",func);
+console.log("nullValue:", nullValue);
+console.log("undefinedvalue:", undefinedvalue);
+console.log("symbolValue:",symbolValue);
+console.log("bigIntValue:",bigIntValue);
+console.log("dateValue:",dateValue);
+
+
+x_ = "5" ;
+y_ = 10;
+
+console.log(x_ === y_);
+console.log(x_ == y_);
+console.log(x_ !== y_);
