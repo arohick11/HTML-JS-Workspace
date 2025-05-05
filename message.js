59c91a08c1cd85fdf1144fa3bcdd38e4e767e43e
arohick11 <arohichakote@gmail.com>
2025-05-05 16:49:19 +0530

    Add files via upload

---
+++ message.js
@@ -0,0 +1,20 @@
+function msg(){
+    alert("Welcome to JS workshop");
+}
+
+let x = 5;
+let y = 10e40;
+let z = "hello";
+let isTrue = "true";
+let arr =[1,2,3,4,5];
+let obj = {name : "John" , age: 30};
+let func = function () {return "Hello World";};
+let nullValue = null;
+let undefinedValue;
+let symbolValue = Symbol("w");
+let bigIntValue = BigInt(123456789012345678901234567890);
+let dateValue = newDate();
+
+
+let demo = `the sum of numbers is ${3+5}`;
+console.log(demo);
